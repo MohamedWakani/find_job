a460a0ab23a9a05f241eca34eb0db4a31e411b6b
MohamedWakani <mohammedwakani9@gmail.com>
2023-03-09 11:36:00 +0000

    updated frontend

--- FrontEnd/src/components/Profile/Profile2.jsx
+++ FrontEnd/src/components/Profile/Profile2.jsx
@@ -14,7 +14,7 @@ export default function Profile2() {
           <div>
             <ul className="text-2xl list-inside capitalize">
             <li className="flex items-center text-blue-600 dark:text-blue-500 my-5">
-        <span className="hidden sm:inline-flex sm:ml-2">description</span>
+         <span className="hidden sm:inline-flex sm:ml-2">description</span>
         <svg aria-hidden="true" className="w-4 h-4 ml-2 sm:ml-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 5l7 7-7 7M5 5l7 7-7 7"></path></svg>
     </li>
             </ul>
